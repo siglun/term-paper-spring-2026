ae2c4329dc9ebad3b4186570a906787a6ea1f485
Sigfrid Lundberg <siggelundberg@gmail.com>
2026-03-24 12:03:26 +0100

    soon there

--- extract-life-tables.pl
+++ extract-life-tables.pl
@@ -3,17 +3,27 @@
 use strict;
 
 my $selection = '^(1751-1754)|(1800-1804).*$';
+my %array;
+
+my $time = "1751-1754";
+my $line = "";
 
 while(my $in = <>) {
     chomp $in;
-    my $time = "";
-    my $line = "";
+    my @data_list = ();
+    
     if($in =~ /$selection/) {
 	my @data = split(/\s+/,$in,10);
-	$time = $data[0];
-#	print $data[1] . "\n";
-#	print $data[6] . "\n";
-	$line = $data[1] . "\t" . $data[6] . "\n";
-	print $line;
+	if($data[0] !~  $time) {
+	    print "" . $line;
+	    $line = "";
+	} else {
+	    $time = $data[0];
+	    push @data_list,$data[6];
+#	    print $data[1] . "\n";
+#	    print $data[6] . "\n";
+	    $line .= $data[1] . "\t" . $data[6] . "\n";
+	}
     }
+
 }
