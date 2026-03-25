28d95be5bdfe6915b2eead1929d8d13725e97a51
Sigfrid Lundberg <siggelundberg@gmail.com>
2026-03-25 12:38:37 +0100

    my programming is rusty

--- extract-life-tables.pl
+++ extract-life-tables.pl
@@ -7,23 +7,42 @@ my %array;
 
 my $time = "1751-1754";
 my $line = "";
+my @data_list = ();
 
 while(my $in = <>) {
     chomp $in;
-    my @data_list = ();
     
     if($in =~ /$selection/) {
 	my @data = split(/\s+/,$in,10);
-	if($data[0] !~  $time) {
-	    print "" . $line;
+#	print "Data 0 $data[ 0]\n";
+#	print "Time $time $data[ 0]\n";
+	if($data[0] !~  /$time/ && @data_list) {
+	    print "in if\n";
+	    $array{$time} = \@data_list;
+	    @data_list = ();
+	    print "list $data_list[0]\n";
 	    $line = "";
-	} else {
+	} elsif ($data[0] =~ /$time/) { 
+	    print "in else\n";
 	    $time = $data[0];
-	    push @data_list,$data[6];
-#	    print $data[1] . "\n";
-#	    print $data[6] . "\n";
-	    $line .= $data[1] . "\t" . $data[6] . "\n";
+	    print "parsed " . $time . " $data[1] " . "$data[6]\n";
+	    push(@data_list,$data[6]);
 	}
     }
 
+
+}
+
+
+my @keys = keys %array;
+
+foreach( my $key = shift(@keys)) {
+    print "shift keys\n";
+    foreach( my @values = @{$array{$key}} ) {
+	print "values\n";
+	for(my $i=0; $i <= $#values; $i++) {
+	    print "$i\t$values[$i]\n";
+	}
+    }
+	
 }
