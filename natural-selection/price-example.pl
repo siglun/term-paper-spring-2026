acdc12ee411abecef8d16130d8e7354ffa10cfb0
Sigfrid Lundberg <siggelundberg@gmail.com>
2026-05-15 10:48:43 +0200

    the simulation generates data

--- natural-selection/price-example.pl
+++ natural-selection/price-example.pl
@@ -2,14 +2,31 @@
 
 use strict;
 
-my @frequencies = (0, 0.5, 1);
-my @fitdif = (1.0, 1.2, 1.6);
+my $q=0.6;
+my $N=100;
 
-foreach my $f (@frequencies) {
-    my $d = shift @fitdif;
-    for(my $i=0;$i<10; $i++) {
-	my $z = $d + rand;
-	print "$f $z\n";
+my $f_within_aa = 0;
+my $f_within_Aa = 0.5;
+my $f_within_AA = 1.0;
+my @fit = (10,50,40);
+my @frequency;
+
+my $frequency_aa = $N*(1-$q)**2;
+my $frequency_Aa = $N*2*$q*(1-$q);
+my $frequency_AA = $N*$q**2;
+
+# print STDERR "$frequency_aa\t$frequency_Aa\t$frequency_AA\n";
+
+for(my $i=1;$i<=$N;$i++) {
+    print $i ." ";
+    if($i <= $frequency_aa) {
+	my $fitness = $fit[0] * (1.5 - rand());
+	print "aa $f_within_aa " . sprintf("%.0f",$fitness)."\n";
+    } elsif($i > $frequency_aa && $i <= $frequency_Aa + $frequency_aa) {
+	my $fitness = $fit[1] * (1.5 - rand());
+	print "Aa $f_within_Aa " . sprintf("%.0f",$fitness)."\n";
+    } else {
+	my $fitness = $fit[2] * (1.5 - rand());
+	print  "AA $f_within_AA " . sprintf("%.0f",$fitness)."\n";
     }
-
 }
